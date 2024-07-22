a701d736d8ef2f2aa57d86cf46166a04e2114bf0
Bhavya S. Patel <bhavyapatel04@gmail.com>
2024-07-22 17:17:47 -0400

    forgot to commit this lolz

--- chip8.c
+++ chip8.c
@@ -1,4 +1,4 @@
-#include "/opt/homebrew/Cellar/sdl2/2.30.3/include/SDL2/SDL.h"
+#include "/opt/homebrew/Cellar/sdl2/2.30.5/include/SDL2/SDL.h"
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
@@ -9,10 +9,21 @@
 #include "chip8_def.h"
 
 int main(int argc, char* argv[]) {
-    
+    //seed the random number generator
+    srand(time(NULL));
+    // Check if the correct number of arguments is provided
+    if (argc != 2) {
+        printf("Usage: ./chip8 path/to/rom\n");
+        return 1;
+    }
+
+    // Store the path to the ROM in a variable
+    char* romPath = argv[1];
+
+    // Rest of the code...
 }
 
-
+ 
 
 
 
---
+++ loadRom.h
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include "instructions.h"
+#include "chip8_def.h"
+
+void loadROM(Chip8* Chip8, char* romPath) {
+    
+}
